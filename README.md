edfe0f82283990afe9d1a68944ed3178d50900a7
Reinier <reinier.vanderhoeven@appythings.nl>
2020-06-05 10:45:26 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,90 @@
+# sap-apim
+Commandline tool to use the management APIs of SAP API Management
+
+Currently supports creating / updating providers and products and generating documentation
+
+# Installation
+```npm i -g sap-apim```
+
+# Configuration
+Create a yaml file with the following contents:
+```yml
+provider:
+  name: example-provider
+  description: This is an example provider
+  isOnPremise: false # default false
+  host: example.com
+  port: 443 # default 443
+  useSsl: true # default true
+  path: /v1/test
+  keyStore: example # optional, useSsl must be true
+  keyAlias: example # optional, useSsl must be true
+  managedByProxy: true # default false
+```
+
+## Use
+Use in combination with [sapim](https://www.npmjs.com/package/sapim)
+
+```sap-apim provider ./manifest.yaml```
+
+
+# To manage products
+Create a yaml file with the following contents:
+```yml
+products:
+  - name: Example-product-silver
+    title: Example product silver
+    description: |
+      Silver version of the example product
+    quota: 2
+    interval: 1
+    timeunit: minute
+    proxies:
+      - echoheaders
+  - name: Example-product-gold
+    title: Example product gold
+    description: |
+      Gold version of the example product
+    quota: 3
+    interval: 1
+    timeunit: minute
+    proxies:
+      - echoheaders
+```
+
+## Use
+Use in combination with [sapim](https://www.npmjs.com/package/sapim)
+
+```sap-apim products ./products.yaml```
+
+# To create documentation for your api proxy
+To be able to see the documentation of an API proxy correctly in the SAP developer portal, 
+the APIProxy requires 2 folders with some documentation content.
+This command will generate those 2 folders from an openapi spec by creating, downloading, extracting the 2 folders and deleting a proxy in SAP.
+Both openapi 3.0 and swagger 2.0 are supported in json and yml format.
+
+## Use
+Use in combination with [sapim](https://www.npmjs.com/package/sapim)
+```
+sap-apim documentation <spec> <outputFolder>
+sap-apim documentation ./openapi.yml ./APIProxy
+```
+
+# To manage KVMs
+This command will create / update / delete the key-values in your KVMs.
+
+Create a yaml file with the following contents, or add this to your current yaml file:
+```yml
+kvms:
+  test:
+    key1: value1
+    key2: value2
+```
+
+## Use
+Use in combination with [sapim](https://www.npmjs.com/package/sapim)
+```
+sap-apim kvms <manifest>
+sap-apim kvms manifest.yaml
+sap-apim kvms manifest.yaml --purgeDeleted // Deletes entries from the KVM that are not in the Manifest
+```
---
+++ src/index.js
@@ -0,0 +1,97 @@
+#!/usr/bin/env node
+const program = require('commander');
+const axios = require('axios');
+const { version, name, description } = require('../package.json');
+
+program.name(name)
+  .version(version, '-v, --version')
+  .requiredOption('-r, --reposlug <reposlug>')
+  .requiredOption('-c, --commit <commit>')
+  .requiredOption('-i, --reportId <reportId>')
+  .requiredOption('-h, --host <host>')
+  .requiredOption('-p, --projectName <projectName>')
+  .requiredOption('-t, --token <token>')
+  .description(description)
+  .action(async () => {
+    try {
+      const res = await axios.get(`${program.host}/api/qualitygates/project_status?projectKey=${program.projectName}`, { auth: { username: program.token, password: '' } });
+      const { projectStatus } = res.data;
+
+      const report = {
+        title: 'Code quality report',
+        details: `Code quality ${projectStatus.status === 'ERROR' ? 'FAILED' : 'PASSED'}`,
+        report_type: 'BUG',
+        reporter: 'AT',
+        result: projectStatus.status === 'ERROR' ? 'FAILED' : 'PASSED',
+        data: projectStatus.conditions.map((condition) => ({
+          title: condition.metricKey,
+          type: 'TEXT',
+          value: condition.actualValue,
+        })),
+      };
+
+      await axios.delete(`https://api.bitbucket.org/2.0/repositories/${program.reposlug}/commit/${program.commit}/reports/${program.reportId}`, {
+        proxy: {
+          host: 'localhost',
+          port: 29418
+        }
+      });
+
+      console.log('deleted report');
+
+      await axios.put(`https://api.bitbucket.org/2.0/repositories/${program.reposlug}/commit/${program.commit}/reports/${program.reportId}`,
+        report, {
+          proxy: {
+            host: 'localhost',
+            port: 29418
+          }
+        });
+
+      console.log('created report');
+
+      const response = await axios.get(`${program.host}/api/issues/search?resolved=false&componentKeys=${program.projectName}&ps=500`, { auth: { username: program.token, password: '' } });
+      const issues = response.data.issues.map((issue) => {
+        let severity = '';
+        switch (issue.severity) {
+          case 'BLOCKER':
+            severity = 'CRITICAL';
+            break;
+          case 'CRITICAL':
+            severity = 'CRITICAL';
+            break;
+          case 'MAJOR':
+            severity = 'HIGH';
+            break;
+          case 'MINOR':
+            severity = 'MEDIUM';
+            break;
+          case 'INFO':
+            severity = 'LOW';
+            break;
+        }
+        return {
+          external_id: issue.key,
+          annotation_type: issue.type,
+          summary: issue.message,
+          details: `effort: ${issue.effort}`,
+          severity,
+          path: issue.component.split(':').pop(),
+          line: issue.line,
+        };
+      });
+
+      await axios.post(`https://api.bitbucket.org/2.0/repositories/${program.reposlug}/commit/${program.commit}/reports/${program.reportId}/annotations`,
+        issues, {
+          proxy: {
+            host: 'localhost',
+            port: 29418
+          }
+        });
+      console.log('created annotations');
+    } catch (e) {
+      e.response ? console.log(e.response.data) : console.log(e.message)
+      process.exit(1);
+    }
+  });
+
+program.parse(process.argv);
